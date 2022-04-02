f2d76c1d8db154035b3213f099590e44392935be
Rey Koll <dephlitheone@gmail.com>
2022-04-02 19:05:55 +0700

    Добавьте файлы проекта.

---
+++ OpenGLTAOLab/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿
+namespace OpenGLTAOLab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GlCanvas = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.task1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dotsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.linesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stripToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.task2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trianglesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trianglesStripToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trianglesFanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.task3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quadsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quadsStripToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.polygonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.labTaskToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GlCanvas
+            // 
+            this.GlCanvas.AccumBits = ((byte)(0));
+            this.GlCanvas.AutoCheckErrors = false;
+            this.GlCanvas.AutoFinish = false;
+            this.GlCanvas.AutoMakeCurrent = true;
+            this.GlCanvas.AutoSwapBuffers = true;
+            this.GlCanvas.BackColor = System.Drawing.Color.Black;
+            this.GlCanvas.ColorBits = ((byte)(32));
+            this.GlCanvas.DepthBits = ((byte)(16));
+            this.GlCanvas.Location = new System.Drawing.Point(-2, 30);
+            this.GlCanvas.Name = "GlCanvas";
+            this.GlCanvas.Size = new System.Drawing.Size(804, 477);
+            this.GlCanvas.StencilBits = ((byte)(0));
+            this.GlCanvas.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(799, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.task1ToolStripMenuItem,
+            this.task2ToolStripMenuItem,
+            this.task3ToolStripMenuItem,
+            this.labTaskToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // task1ToolStripMenuItem
+            // 
+            this.task1ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dotsToolStripMenuItem,
+            this.linesToolStripMenuItem,
+            this.stripToolStripMenuItem,
+            this.loopToolStripMenuItem});
+            this.task1ToolStripMenuItem.Name = "task1ToolStripMenuItem";
+            this.task1ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.task1ToolStripMenuItem.Text = "Task 1";
+            // 
+            // dotsToolStripMenuItem
+            // 
+            this.dotsToolStripMenuItem.Name = "dotsToolStripMenuItem";
+            this.dotsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.dotsToolStripMenuItem.Text = "Dots";
+            this.dotsToolStripMenuItem.Click += new System.EventHandler(this.dotsToolStripMenuItem_Click);
+            // 
+            // linesToolStripMenuItem
+            // 
+            this.linesToolStripMenuItem.Name = "linesToolStripMenuItem";
+            this.linesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.linesToolStripMenuItem.Text = "Lines";
+            this.linesToolStripMenuItem.Click += new System.EventHandler(this.linesToolStripMenuItem_Click);
+            // 
+            // stripToolStripMenuItem
+            // 
+            this.stripToolStripMenuItem.Name = "stripToolStripMenuItem";
+            this.stripToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.stripToolStripMenuItem.Text = "Strip";
+            this.stripToolStripMenuItem.Click += new System.EventHandler(this.stripToolStripMenuItem_Click);
+            // 
+            // loopToolStripMenuItem
+            // 
+            this.loopToolStripMenuItem.Name = "loopToolStripMenuItem";
+            this.loopToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loopToolStripMenuItem.Text = "Loop";
+            this.loopToolStripMenuItem.Click += new System.EventHandler(this.loopToolStripMenuItem_Click);
+            // 
+            // task2ToolStripMenuItem
+            // 
+            this.task2ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.trianglesToolStripMenuItem,
+            this.trianglesStripToolStripMenuItem,
+            this.trianglesFanToolStripMenuItem});
+            this.task2ToolStripMenuItem.Name = "task2ToolStripMenuItem";
+            this.task2ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.task2ToolStripMenuItem.Text = "Task2";
+            // 
+            // trianglesToolStripMenuItem
+            // 
+            this.trianglesToolStripMenuItem.Name = "trianglesToolStripMenuItem";
+            this.trianglesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.trianglesToolStripMenuItem.Text = "Triangles";
+            this.trianglesToolStripMenuItem.Click += new System.EventHandler(this.trianglesToolStripMenuItem_Click);
+            // 
+            // trianglesStripToolStripMenuItem
+            // 
+            this.trianglesStripToolStripMenuItem.Name = "trianglesStripToolStripMenuItem";
+            this.trianglesStripToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.trianglesStripToolStripMenuItem.Text = "Triangles Strip";
+            this.trianglesStripToolStripMenuItem.Click += new System.EventHandler(this.trianglesStripToolStripMenuItem_Click);
+            // 
+            // trianglesFanToolStripMenuItem
+            // 
+            this.trianglesFanToolStripMenuItem.Name = "trianglesFanToolStripMenuItem";
+            this.trianglesFanToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.trianglesFanToolStripMenuItem.Text = "Triangles Fan";
+            this.trianglesFanToolStripMenuItem.Click += new System.EventHandler(this.trianglesFanToolStripMenuItem_Click);
+            // 
+            // task3ToolStripMenuItem
+            // 
+            this.task3ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quadsToolStripMenuItem,
+            this.quadsStripToolStripMenuItem,
+            this.polygonToolStripMenuItem});
+            this.task3ToolStripMenuItem.Name = "task3ToolStripMenuItem";
+            this.task3ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.task3ToolStripMenuItem.Text = "Task 3";
+            // 
+            // quadsToolStripMenuItem
+            // 
+            this.quadsToolStripMenuItem.Name = "quadsToolStripMenuItem";
+            this.quadsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.quadsToolStripMenuItem.Text = "Quads";
+            this.quadsToolStripMenuItem.Click += new System.EventHandler(this.quadsToolStripMenuItem_Click);
+            // 
+            // quadsStripToolStripMenuItem
+            // 
+            this.quadsStripToolStripMenuItem.Name = "quadsStripToolStripMenuItem";
+            this.quadsStripToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.quadsStripToolStripMenuItem.Text = "Quads Strip";
+            this.quadsStripToolStripMenuItem.Click += new System.EventHandler(this.quadsStripToolStripMenuItem_Click);
+            // 
+            // polygonToolStripMenuItem
+            // 
+            this.polygonToolStripMenuItem.Name = "polygonToolStripMenuItem";
+            this.polygonToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.polygonToolStripMenuItem.Text = "Polygon";
+            this.polygonToolStripMenuItem.Click += new System.EventHandler(this.polygonToolStripMenuItem_Click);
+            // 
+            // labTaskToolStripMenuItem
+            // 
+            this.labTaskToolStripMenuItem.Name = "labTaskToolStripMenuItem";
+            this.labTaskToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.labTaskToolStripMenuItem.Text = "Lab Task";
+            this.labTaskToolStripMenuItem.Click += new System.EventHandler(this.labTaskToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 504);
+            this.Controls.Add(this.GlCanvas);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl GlCanvas;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem task1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dotsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem linesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stripToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem task2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem trianglesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem trianglesStripToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem trianglesFanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem task3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quadsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quadsStripToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem polygonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem labTaskToolStripMenuItem;
+    }
+}
+
---
+++ OpenGLTAOLab/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System.Windows.Forms;
+using Tao.OpenGl;
+
+namespace OpenGLTAOLab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            GlCanvas.InitializeContexts();
+        }
+
+        private void Clear()
+        {
+            Gl.glViewport(0, 0, GlCanvas.Width, GlCanvas.Height);
+            Gl.glClearColor(1f, 1f, 1f, 1);
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+        }
+
+        private void DrawTriangle()
+        {
+            Gl.glBegin(Gl.GL_TRIANGLES);
+            Gl.glColor3f(0, 1, 0);
+            Gl.glVertex3f(-0.5f, 0.5f, 0);
+            Gl.glColor3f(1, 0, 0);
+            Gl.glVertex3f(0.5f, 0.5f, 0);
+            Gl.glColor3f(0, 0, 1);
+            Gl.glVertex3f(0, -0.5f, 0);
+            Gl.glEnd();
+        }
+
+        private void DrawDot()
+        {
+            Gl.glColor3f(0, 0, 0);
+            Gl.glPointSize(20f);
+            Gl.glEnable(Gl.GL_POINT_SMOOTH);
+            Gl.glBegin(Gl.GL_POINTS);
+            Gl.glVertex2f(0, 0);
+            Gl.glVertex2f(0.5f, 0.5f);
+            Gl.glVertex2f(-0.5f, 0.5f);
+            Gl.glVertex2f(0.5f, -0.5f);
+            Gl.glVertex2f(-0.5f, -0.5f);
+            Gl.glEnd();
+        }
+
+        private void DrawLines()
+        {
+            Gl.glLineWidth(7);
+            Gl.glColor3f(0, 0, 1);
+            Gl.glBegin(Gl.GL_LINES);
+            Gl.glVertex2f(-1.0f, 1.0f);
+            Gl.glVertex2f(1.0f, -1.0f);
+            Gl.glVertex2f(1.0f, 1.0f);
+            Gl.glVertex2f(-1.0f, -1.0f);
+            Gl.glEnd();
+        }
+
+        private void DrawStripLines()
+        {
+            Gl.glLineWidth(7);
+            Gl.glColor3f(1, 0, 1);
+            Gl.glBegin(Gl.GL_LINE_STRIP);
+
+            Gl.glVertex2f(0.5f, 0.5f); 
+            Gl.glVertex2f(-0.5f, 0.5f); 
+                                   
+            Gl.glVertex2f(0.5f, -0.5f); 
+            Gl.glVertex2f(-0.5f, -0.5f); 
+            Gl.glEnd();
+        }
+
+        private void DrawLinesLoop()
+        {
+            Gl.glLineWidth(7);
+            Gl.glColor3f(1, 0, 1);
+            Gl.glBegin(Gl.GL_LINE_LOOP);
+            Gl.glVertex2f(0.5f, 0.5f);
+            Gl.glVertex2f(-0.5f, 0.5f);
+            Gl.glVertex2f(0.5f, -0.5f);
+            Gl.glVertex2f(-0.5f, -0.5f);
+            Gl.glEnd();
+        }
+
+        private void DrawTriangleFan()
+        {
+            Gl.glLineWidth(7);
+            Gl.glColor3f(1, 0, 1);
+            Gl.glBegin(Gl.GL_TRIANGLE_FAN);
+            Gl.glVertex2f(0.0f, 0.0f);
+            Gl.glVertex2f(0.5f, 0.5f);
+            Gl.glVertex2f(-0.5f, 0.5f);
+            Gl.glVertex2f(0.5f, -0.5f);
+            Gl.glVertex2f(-0.5f, -0.5f);
+            Gl.glEnd();
+        }
+
+        private void DrawQuads()
+        {
+            Gl.glBegin(Gl.GL_QUADS);
+            Gl.glColor3f(1, 0, 1);
+            Gl.glVertex2f(-0.5f, 0.5f);
+            Gl.glVertex2f(-0.5f, -0.5f);
+            Gl.glVertex2f(0.5f, -0.5f);
+            Gl.glColor3f(1, 1, 1);
+            Gl.glVertex2f(0.5f, 0.5f);
+            Gl.glEnd();
+        }
+
+        private void DrawQuadsStrip()
+        {
+            Gl.glColor3f(0, 0, 0);
+            Gl.glBegin(Gl.GL_QUAD_STRIP);
+            Gl.glVertex2f(-0.5f, 0.5f);
+            Gl.glVertex2f(-0.5f, -0.5f);
+            Gl.glVertex2f(0.5f, 0.5f);
+            Gl.glColor3f(1, 1, 1);
+            Gl.glVertex2f(0.5f, -0.3f);
+            Gl.glEnd();
+        }
+
+        private void DrawPolygon()
+        {
+            Gl.glColor3f(0, 0, 0);
+            Gl.glBegin(Gl.GL_POLYGON);
+            Gl.glVertex2f(-0.3f, 0.5f);
+            Gl.glVertex2f(0.3f, 0.5f);
+            Gl.glVertex2f(0.6f, 0.0f);
+            Gl.glVertex2f(0.3f, -0.5f);
+            Gl.glColor3f(1, 1, 1);
+            Gl.glVertex2f(-0.3f, -0.5f);
+            Gl.glColor3f(1, 1, 1);
+            Gl.glVertex2f(-0.6f, 0.0f);
+            Gl.glEnd();
+        }
+
+        private void DrawTriangleStrip()
+        {
+            Gl.glColor3f(0, 0, 0);
+            Gl.glBegin(Gl.GL_TRIANGLE_STRIP);
+            Gl.glVertex2f(-0.5f, 0.0f);
+            Gl.glColor3f(1, 0, 1);
+            Gl.glVertex2f(0.0f, -0.7f);
+            Gl.glVertex2f(0.0f, 0.7f);
+            Gl.glVertex2f(0.5f, 0.0f);
+            Gl.glEnd();
+        }
+
+        private void Task()
+        {
+            Gl.glColor3f(0, 0, 0);
+            Gl.glPointSize(10f);
+            Gl.glEnable(Gl.GL_POINT_SMOOTH);
+            Gl.glBegin(Gl.GL_POINTS);
+            Gl.glVertex2f(0, 0);
+            Gl.glEnd();
+            Gl.glColor3f(0, 0, 0);
+            Gl.glLineWidth(2);
+            Gl.glBegin(Gl.GL_LINES);
+
+            Gl.glVertex2f(0.8f, 0.8f);
+            Gl.glVertex2f(-0.8f, 0.8f);
+
+            Gl.glVertex2f(0.8f, -0.8f);
+            Gl.glVertex2f(-0.8f, -0.8f);
+
+            Gl.glVertex2f(-0.8f, 0.8f);
+            Gl.glVertex2f(-0.8f, -0.8f);
+
+            Gl.glVertex2f(0.8f, -0.8f);
+            Gl.glVertex2f(0.8f, 0.8f);
+
+            Gl.glVertex2f(-0.5f, 0.5f);
+            Gl.glVertex2f(-0.5f, -0.5f);
+
+            Gl.glVertex2f(-0.5f, -0.5f);
+            Gl.glVertex2f(0.3f, -0.3f);
+
+            Gl.glVertex2f(0.3f, -0.3f);
+            Gl.glVertex2f(0.0f, 0.0f);
+
+            Gl.glVertex2f(0.0f, 0.0f);
+            Gl.glVertex2f(0.3f, 0.3f);
+
+            Gl.glVertex2f(0.3f, 0.3f);
+            Gl.glVertex2f(-0.5f, 0.5f);
+
+            Gl.glEnd();
+        }
+
+        private void labTaskToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            Clear();
+            Task();
+            GlCanvas.Invalidate();
+        }
+
+        private void dotsToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            Clear();
+            DrawDot();
+            GlCanvas.Invalidate();
+        }
+
+        private void linesToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            Clear();
+            DrawLines();
+            GlCanvas.Invalidate();
+        }
+
+        private void stripToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            Clear();
+            DrawStripLines();
+            GlCanvas.Invalidate();
+        }
+
+        private void loopToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            Clear();
+            DrawLinesLoop();
+            GlCanvas.Invalidate();
+        }
+
+        private void trianglesToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            Clear();
+            DrawTriangle();
+            GlCanvas.Invalidate();
+        }
+
+        private void trianglesStripToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            Clear();
+            DrawTriangleStrip();
+            GlCanvas.Invalidate();
+        }
+
+        private void trianglesFanToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            Clear();
+            DrawTriangleFan();
+            GlCanvas.Invalidate();
+        }
+
+        private void quadsToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            Clear();
+            DrawQuads();
+            GlCanvas.Invalidate();
+        }
+
+        private void quadsStripToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            Clear();
+            DrawQuadsStrip();
+            GlCanvas.Invalidate();
+        }
+
+        private void polygonToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            Clear();
+            DrawPolygon();
+            GlCanvas.Invalidate();
+        }
+    }
+}
